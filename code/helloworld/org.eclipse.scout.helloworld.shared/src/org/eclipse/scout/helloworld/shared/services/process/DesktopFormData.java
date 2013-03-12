bef0deb1d165c36d0242b6e2bd6ff3efd7c6c659
Matthias Zimmermann <matthiaszimmermanntest>
2013-03-12 22:52:40 +0100

    cleanup compile artefacts

--- code/helloworld/org.eclipse.scout.helloworld.shared/src/org/eclipse/scout/helloworld/shared/services/process/DesktopFormData.java
+++ code/helloworld/org.eclipse.scout.helloworld.shared/src/org/eclipse/scout/helloworld/shared/services/process/DesktopFormData.java
@@ -1,32 +1,10 @@
 package org.eclipse.scout.helloworld.shared.services.process;
 
-import org.eclipse.scout.rt.shared.data.form.ValidationRule;
-import org.eclipse.scout.rt.shared.data.form.fields.AbstractValueFieldData;
 import org.eclipse.scout.rt.shared.data.form.AbstractFormData;
 
 public class DesktopFormData extends AbstractFormData {
   private static final long serialVersionUID = 1L;
 
   public DesktopFormData() {
-  }
-
-  public Message getMessage() {
-    return getFieldByClass(Message.class);
-  }
-
-  public static class Message extends AbstractValueFieldData<String> {
-    private static final long serialVersionUID = 1L;
-
-    public Message() {
-    }
-
-    /**
-     * list of derived validation rules.
-     */
-    @Override
-    protected void initValidationRules(java.util.Map<String, Object> ruleMap) {
-      super.initValidationRules(ruleMap);
-      ruleMap.put(ValidationRule.MAX_LENGTH, 4000);
-    }
   }
 }
--- code/helloworld/org.eclipse.scout.helloworld.shared/src/org/eclipse/scout/helloworld/shared/services/process/IDesktopProcessService.java
+++ code/helloworld/org.eclipse.scout.helloworld.shared/src/org/eclipse/scout/helloworld/shared/services/process/IDesktopProcessService.java
@@ -1,10 +1,9 @@
 package org.eclipse.scout.helloworld.shared.services.process;
 
-import org.eclipse.scout.service.IService;
-import org.eclipse.scout.helloworld.shared.services.process.DesktopFormData;
 import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.service.IService;
 
-public interface IDesktopProcessService extends IService{
+public interface IDesktopProcessService extends IService {
 
   public DesktopFormData load(DesktopFormData formData) throws ProcessingException;
 }
